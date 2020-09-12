bd77b5d49c02cd523adac929228a544d2452b325
abhinavvarshney <abhinav.varshney@geminisolutions.in>
2020-09-12 15:35:20 +0530

    first commit

---
+++ build/precache-manifest.8fba0a77e10681bc5ea9d1675fc85625.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "616d51c2bf5b822e0a61",
+    "url": "/weshowedup/static/css/main.0c63f251.chunk.css"
+  },
+  {
+    "revision": "616d51c2bf5b822e0a61",
+    "url": "/weshowedup/static/js/main.d138d8e3.chunk.js"
+  },
+  {
+    "revision": "5230e7778cf06131e2c3",
+    "url": "/weshowedup/static/js/runtime~main.c390a4eb.js"
+  },
+  {
+    "revision": "adec801a0b087f6a9872",
+    "url": "/weshowedup/static/js/2.d9678f95.chunk.js"
+  },
+  {
+    "revision": "c6e0bb02f5ed58ad4bbd53cb6a93f904",
+    "url": "/weshowedup/static/media/app_dev.c6e0bb02.jfif"
+  },
+  {
+    "revision": "8ffcae8d7c9b7cc8bddb514b87e930f3",
+    "url": "/weshowedup/static/media/home_image_one.8ffcae8d.png"
+  },
+  {
+    "revision": "d5a709db6127334379b6e8a3c6f2ce66",
+    "url": "/weshowedup/index.html"
+  }
+];
